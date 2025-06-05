9a66f3485fef7f797db92ebcb11cceeb8d19c9f8
Fyodor Smorodin <fjodor.smorodins@student.nhlstenden.com>
2025-06-05 13:59:05 +0200

    fixing ScanOptions class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,6 @@ dependencies {
     implementation("androidx.cardview:cardview:1.0.0")
     implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
 
     annotationProcessor(libs.room.compiler)
 
